bd37615ff473313ee676b872841c3c26a86ecdcf
bharat moger <03bharatmgr@gmail.com>
2026-07-31 11:42:44 +0530

    made some changes

--- 01-Arrays/JumpGame.java
+++ 01-Arrays/JumpGame.java
@@ -1,21 +1,23 @@
 public class JumpGame
 {
     public static void main(String[] args) {
-        int[] nums = {2,3,1,1,4};
+        int[] nums = {3,2,1,0,4};
         System.out.println(targetReachable(nums));
     }
 
     public static boolean targetReachable(int[] nums)
     {
         int maxDistance=0;
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++)
+        {
             if (maxDistance <i) {
                 return false;
             }
-            maxDistance = Math.max(maxDistance, i+nums[i]);
-            if (maxDistance >= nums.length - 1) {
+
+            maxDistance=Math.max(maxDistance, i+nums[i]);
+
+            if(maxDistance>= nums.length-1)
                 return true;
-            }
         }
         return true;
     }
